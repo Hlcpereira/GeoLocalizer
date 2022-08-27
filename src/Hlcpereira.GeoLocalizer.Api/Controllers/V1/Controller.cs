7a9d27e112e79468bca671a639530d5298736ae0
Henrique Pereira <hlcpereira@pixelexperience.org>
2022-08-27 18:05:29 -0300

    GeoLocalizer: Initial Commit
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

---
+++ src/Hlcpereira.GeoLocalizer.Api/Controllers/V1/Controller.cs
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2022 - Henrique Pereira/Hlcpereira
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hlcpereira.GeoLocalizer.Api.Controllers.V1
+{
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "GeoLocation")]
+    [Route("[controller]")]
+    public class Controller : ControllerBase
+    {
+
+    }
+}
